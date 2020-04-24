c595a6bfd588671bc8fede052762f31f265f95ce
Henry Chang <henryc@google.com>
2020-04-24 08:43:26 -0700

    reword descripition of script

--- bin/asd-prereq-checker.sh
+++ bin/asd-prereq-checker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# This script will check that prerequisites exist to ensure that
-# Anthos Sample Deployment will be successfully deployed.
+# This script checks that prerequisites exist to ensure that
+# the Anthos Sample Deployment is successfully deployed.
 #
 # Example usage:
 # ./asd-prereq-checker.sh --network foo_network
